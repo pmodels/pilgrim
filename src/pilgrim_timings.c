560e734d41b72581ed7e05120184835ff4f0b629
Chen Wang <wangvsa@gmail.com>
2020-12-07 09:06:25 -0800

    Change comments

--- src/pilgrim_timings.c
+++ src/pilgrim_timings.c
@@ -1,11 +1,20 @@
 /**
- * Binary search tree (BST) to keep
- * existing duration bins.
- *
- * Each function has its own BST
+ * Interval ID and Duration ID stored in the key
  */
 #include <math.h>
+#include "pilgrim.h"
 #include "pilgrim_timings.h"
+#include "uthash.h"
+
+typedef struct IntervalHash_t {
+    void *key;
+    int key_len;
+    double tstart;
+    UT_hash_handle hh;
+} IntervalHash;
+
+static IntervalHash *interval_table = NULL;
+
 
 int get_id(double val) {
     double base = 1.1;
@@ -13,7 +22,45 @@ int get_id(double val) {
     return res;
 }
 
-int get_interval_id(double interval) {
+
+double get_interval(const Record *record) {
+    int i;
+    int key_len = sizeof(record->func_id);
+    for(i = 0; i < record->arg_count; i++)
+        key_len += record->arg_sizes[i];
+
+    void *key = dlmalloc(key_len);
+    memcpy(key, &(record->func_id), sizeof(record->func_id));
+
+    int pos = sizeof(record->func_id);
+    for(i = 0; i < record->arg_count; i++) {
+        memcpy(key+pos, record->args[i], record->arg_sizes[i]);
+        pos += record->arg_sizes[i];
+    }
+
+    IntervalHash *entry;
+    HASH_FIND(hh, interval_table, key, key_len, entry);
+
+    double interval;
+    if(entry) {                         // Found
+        dlfree(key);
+        interval = record->tstart - entry->tstart;
+    } else {                            // Not exist, add to hash table
+        entry = (IntervalHash*) dlmalloc(sizeof(IntervalHash));
+        entry->key = key;
+        entry->key_len = key_len;
+        entry->tstart = record->tstart;
+
+        HASH_ADD_KEYPTR(hh, interval_table, entry->key, key_len, entry);
+
+        interval = -1;
+    }
+
+    return interval;
+}
+
+int get_interval_id(const Record* record) {
+    double interval = get_interval(record);
     return get_id(interval);
 }
 

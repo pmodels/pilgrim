8b12d9e61c7e59561e6ec4227182003733d7bf22
Chen Wang <wangvsa@gmail.com>
2021-06-08 09:55:28 -0500

    The app generator works for a simple Pingpong program now
    Signed-off-by: Chen Wang <wangvsa@gmail.com>

---
+++ test/pingpong.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) by Argonne National Laboratory
+ *     See COPYRIGHT in top-level directory
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <mpi.h>
+
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+    int rank, size;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+
+    assert(size >= 2);
+
+    int* data;
+    if(rank == 0) {
+        data = malloc(sizeof(int)*3);
+        for(int i = 0; i < 3;i ++)
+            MPI_Send(data, 3, MPI_INT, 1, 123, MPI_COMM_WORLD);
+    }
+    else if(rank == 1) {
+        data = malloc(sizeof(int)*5);
+        for(int i = 0; i < 3;i ++)
+            MPI_Recv(&(data[2]), 3, MPI_INT, 0, 123, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    } else {
+        for(int i = 0; i < 10;i ++)
+            MPI_Comm_size(MPI_COMM_WORLD, &size);
+    }
+
+
+    free(data);
+    MPI_Finalize();
+    return 0;
+}

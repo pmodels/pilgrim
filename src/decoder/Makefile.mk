cef2914694b90456ff2e291d146405b12962f168
Chen Wang <wangvsa@gmail.com>
2021-06-14 10:37:57 -0500

    Rename decoder to app_generator
    Signed-off-by: Chen Wang <wangvsa@gmail.com>

--- src/decoder/Makefile.mk
+++ src/decoder/Makefile.mk
@@ -4,3 +4,17 @@
 ##
 
 AM_CPPFLAGS += -I$(top_srcdir)/src
+
+pilgrim_app_generator_SOURCES += \
+	src/decoder/pilgrim_app_generator.c \
+	src/decoder/pilgrim_metadata_decoder.c \
+	src/decoder/pilgrim_cfg_decoder.c src/decoder/pilgrim_cst_decoder.c \
+	src/decoder/pilgrim_read_args.c src/decoder/pilgrim_read_args_special.c \
+	src/dlmalloc.c
+
+pilgrim2text_SOURCES += \
+	src/decoder/pilgrim2text.c \
+	src/decoder/pilgrim_metadata_decoder.c \
+	src/decoder/pilgrim_cfg_decoder.c src/decoder/pilgrim_cst_decoder.c \
+	src/decoder/pilgrim_read_args.c src/decoder/pilgrim_read_args_special.c \
+	src/dlmalloc.c

e2ab969541bdcc085766cc46c41e3929c76d936f
Chen Wang <wangvsa@gmail.com>
2021-07-01 14:38:11 -0500

    Bug fix
    Signed-off-by: Chen Wang <wangvsa@gmail.com>

--- test/generator/sendrecv.c
+++ test/generator/sendrecv.c
@@ -11,8 +11,8 @@ int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
     int rank, size;
 
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     assert(size >= 2);
 
@@ -21,18 +21,19 @@ int main(int argc, char** argv) {
         data = malloc(sizeof(int)*3);
         for(int i = 0; i < 3;i ++)
             MPI_Send(data, 3, MPI_INT, 1, 123, MPI_COMM_WORLD);
+        free(data);
     }
     else if(rank == 1) {
         data = malloc(sizeof(int)*5);
         for(int i = 0; i < 3;i ++)
             MPI_Recv(&(data[2]), 3, MPI_INT, 0, 123, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        free(data);
     } else {
         for(int i = 0; i < 10;i ++)
             MPI_Comm_size(MPI_COMM_WORLD, &size);
     }
 
 
-    free(data);
     MPI_Finalize();
     return 0;
 }
